e10c70dc4d5641ff05028b558dd221eb2bbf81b1
Jason Norwood-Young <jason@10layer.com>
2020-05-15 13:38:30 +0000

    Cast boolean to string

--- modules/piano-composer/js/piano.js
+++ modules/piano-composer/js/piano.js
@@ -4,7 +4,7 @@ tp.push(["setContentAuthor", revengine_piano_composer_vars.author]);
 tp.push(["setContentSection", revengine_piano_composer_vars.sections]);
 tp.push(["setTags", revengine_piano_composer_vars.tags]);
 tp.push(["setCustomParam", "type", "premium", "content"])
-tp.push(["setCustomVariable", "logged_in", !!(revengine_piano_composer_vars.logged_in)]);
+tp.push(["setCustomVariable", "logged_in", String(!!(revengine_piano_composer_vars.logged_in))]);
 tp.push(["setCustomVariable", "memberships", revengine_piano_composer_vars.memberships]);
 (function(src){
     var a=document.createElement("script");
