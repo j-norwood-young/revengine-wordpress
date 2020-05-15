9934b73e795218e988d974e1d83c826e4768d7ec
Jason Norwood-Young <jason@10layer.com>
2020-05-15 14:33:17 +0000

    Correct live name

--- modules/piano-composer/js/piano.js
+++ modules/piano-composer/js/piano.js
@@ -13,4 +13,4 @@ tp.push(["setCustomVariable", "memberships", revengine_piano_composer_vars.membe
     a.src=src;
     var b=document.getElementsByTagName("script")[0];
     b.parentNode.insertBefore(a,b)
-})(`https://${(revengine_piano_composer_vars.revengine_piano_sandbox_mode) ? `sandbox.tinypass.com` : `api.tinypass.com`}/xbuilder/experience/load?aid=${revengine_piano_composer_vars.revengine_piano_id }`);
+})(`https://${(revengine_piano_composer_vars.revengine_piano_sandbox_mode) ? `sandbox.tinypass.com` : `experience.tinypass.com`}/xbuilder/experience/load?aid=${revengine_piano_composer_vars.revengine_piano_id }`);
