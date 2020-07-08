1abe481315b7b5616f3b8bcd38394c998b98a178
Jason Norwood-Young <jason@10layer.com>
2020-07-08 08:13:21 +0000

    Added woocommerce_memberships endpoint

--- modules/revengine-api/revengine-api.php
+++ modules/revengine-api/revengine-api.php
@@ -10,6 +10,116 @@ class RevEngineAPI {
         "revengine_api_types"
     ];
 
+    private $order_filtered_fields = [
+        "id",
+        "date_created",
+        "date_modified",
+        "date_completed",
+        "date_paid",
+        "total",
+        "customer_id",
+        "order_key",
+        "user",
+        "payment_method",
+        "customer_ip_address",
+        "customer_user_agent",
+        "products",
+    ];
+
+    private $subscription_filtered_fields = [
+        "id",
+        "status",
+        "date_created",
+        "date_modified",
+        "date_completed",
+        "schedule_start",
+        "scedule_cancelled",
+        "schedule_next_payment",
+        "schedule_payment_retry",
+        "date_paid",
+        "total",
+        "customer_id",
+        "order_key",
+        "payment_method",
+        "customer_ip_address",
+        "customer_user_agent",
+        "created_via",
+        "customer_note",
+        "billing_period",
+        "billing_interval",
+        "suspension_count",
+        "requires_manual_renewal",
+        "cancelled_email_sent",
+        "products",
+        "meta_data"
+    ];
+
+    private $product_filtered_fields = [
+        "id",
+        "name",
+        "slug",
+        "date_created",
+        "date_modified",
+    ];
+
+    private $membership_filtered_fields = [
+        "id",
+        "customer_id",
+        "status",
+        "start_date",
+        "end_date",
+        "cancelled_date",
+        "paused_intervals",
+        "date_created",
+        "date_modified",
+        "order",
+        "product"
+    ];
+
+    private $user_filtered_fields = [
+        "id",
+        "billing_phone",
+        "current_login",
+        "description",
+        "display_name",
+        "dm-ad-free-interacted",
+        "dm-ad-free-toggle",
+        "dm-status-user",
+        "facebook",
+        "first_name",
+        "followAuthor",
+        "followedAuthors",
+        "followAuthorNotice",
+        "gender",
+        "googleplus",
+        "last_login",
+        "last_name",
+        "last_update",
+        "nickname",
+        "paying_customer",
+        "rs_saved_for_later",
+        "saveForLaterNotice",
+        "session_tokens",
+        "twitter",
+        "user_dob",
+        "user_email",
+        "user_facebook",
+        "user_industry",
+        "user_linkedin",
+        "user_login",
+        "user_nicename",
+        "user_pass",
+        "user_registered",
+        "user_status",
+        "user_twitter",
+        "user_url",
+        "wc_last_active",
+        "wp_capabilities",
+        "wp_user_level",
+        "wsl_current_provider",
+        "wsl_current_user_image",
+    ];
+
     function __construct($revengine_globals) {
         $this->revengine_globals = &$revengine_globals;
         add_action('admin_init', [ $this, 'register_settings' ]);
@@ -46,22 +156,27 @@ class RevEngineAPI {
             'methods' => 'GET',
             'callback' => [$this, 'get_posts'],
             'permission_callback' => [$this, 'check_access']
-        ) );
+        ));
         register_rest_route( 'revengine/v1', '/users', array(
             'methods' => 'GET',
             'callback' => [$this, 'get_users'],
             'permission_callback' => [$this, 'check_access']
-        ) );
+        ));
         register_rest_route( 'revengine/v1', '/woocommerce_orders', array(
             'methods' => 'GET',
             'callback' => [$this, 'get_woocommerce_orders'],
             'permission_callback' => [$this, 'check_access']
-        ) );
+        ));
         register_rest_route( 'revengine/v1', '/woocommerce_subscriptions', array(
             'methods' => 'GET',
             'callback' => [$this, 'get_woocommerce_subscriptions'],
             'permission_callback' => [$this, 'check_access']
-        ) );
+        ));
+        register_rest_route( 'revengine/v1', '/woocommerce_memberships', array(
+            'methods' => 'GET',
+            'callback' => [$this, 'get_woocommerce_memberships'],
+            'permission_callback' => [$this, 'check_access']
+        ));
     }
 
     function filter_fields($data, $filtered_fields) {
@@ -185,49 +300,6 @@ class RevEngineAPI {
     function get_users(WP_REST_Request $request) {
         global $wpdb;
         global $wp;
-        $filtered_fields = [
-            "id",
-            "billing_phone",
-            "current_login",
-            "description",
-            "display_name",
-            "dm-ad-free-interacted",
-            "dm-ad-free-toggle",
-            "dm-status-user",
-            "facebook",
-            "first_name",
-            "followAuthor",
-            "followedAuthors",
-            "followAuthorNotice",
-            "gender",
-            "googleplus",
-            "last_login",
-            "last_name",
-            "last_update",
-            "nickname",
-            "paying_customer",
-            "rs_saved_for_later",
-            "saveForLaterNotice",
-            "session_tokens",
-            "twitter",
-            "user_dob",
-            "user_email",
-            "user_facebook",
-            "user_industry",
-            "user_linkedin",
-            "user_login",
-            "user_nicename",
-            "user_pass",
-            "user_registered",
-            "user_status",
-            "user_twitter",
-            "user_url",
-            "wc_last_active",
-            "wp_capabilities",
-            "wp_user_level",
-            "wsl_current_provider",
-            "wsl_current_user_image",
-        ];
         $date_fields = [
             "last_update",
             "last_login",
@@ -255,7 +327,7 @@ class RevEngineAPI {
             }
             // print_r($user);
             $user = $this->normalise_fields($user);
-            $user = $this->filter_fields($user, $filtered_fields);
+            $user = $this->filter_fields($user, $this->user_filtered_fields);
             foreach($date_fields as $date_field) {
                 if ($user[$date_field]) {
                     $user[$date_field] = $this->convert_to_date($user[$date_field]);
@@ -283,21 +355,6 @@ class RevEngineAPI {
 
     function get_woocommerce_orders(WP_REST_Request $request) {
         global $wp;
-        $filtered_fields = [
-            "id",
-            "date_created",
-            "date_modified",
-            "date_completed",
-            "date_paid",
-            "total",
-            "customer_id",
-            "order_key",
-            "user",
-            "payment_method",
-            "customer_ip_address",
-            "customer_user_agent",
-            "products",
-        ];
         $per_page = intval($request->get_param( "per_page") ?? 10);
         $page = intval($request->get_param( "page") ?? 1);
         $args = ([
@@ -343,7 +400,7 @@ class RevEngineAPI {
                         }
                     }
                 }
-                $order_data = $this->filter_fields($order_data, $filtered_fields);
+                $order_data = $this->filter_fields($order_data, $this->order_filtered_fields);
                 $order_data = $this->normalise_fields($order_data);
                 $result[] = $order_data;
             } catch(Exception $err) {
@@ -370,33 +427,6 @@ class RevEngineAPI {
 
     function get_woocommerce_subscriptions(WP_REST_Request $request) {
         global $wp;
-        $filtered_fields = [
-            "id",
-            "status",
-            "date_created",
-            "date_modified",
-            "date_completed",
-            "schedule_start",
-            "scedule_cancelled",
-            "schedule_next_payment",
-            "schedule_payment_retry",
-            "date_paid",
-            "total",
-            "customer_id",
-            "order_key",
-            "payment_method",
-            "customer_ip_address",
-            "customer_user_agent",
-            "created_via",
-            "customer_note",
-            "billing_period",
-            "billing_interval",
-            "suspension_count",
-            "requires_manual_renewal",
-            "cancelled_email_sent",
-            "products",
-            "meta_data"
-        ];
         $per_page = intval($request->get_param( "per_page") ?? 10);
         $page = intval($request->get_param( "page") ?? 1);
         $args = ([
@@ -446,7 +476,7 @@ class RevEngineAPI {
             }
             $filtered_subscription_data = [];
             foreach($subscription_data as $key => $val) {
-                if (in_array($key, $filtered_fields)) {
+                if (in_array($key, $this->subscription_filtered_fields)) {
                     $filtered_subscription_data[$key] = $val;
                 }
             }
@@ -456,6 +486,91 @@ class RevEngineAPI {
                 }
             }
             $result[] = $filtered_subscription_data;
+        }
+        $next_url = add_query_arg( ["page" => $page + 1, "per_page" => $per_page], home_url($wp->request) );
+        $prev_url = add_query_arg( ["page" => $page - 1, "per_page" => $per_page], home_url($wp->request) );
+        $data = [
+            "page" => $page,
+            "per_page" => $per_page,
+            "page_count" => $page_count,
+            "total_count" => $count,
+        ];
+        if ($page > 1) {
+            $data["prev"] = $prev_url;
+        }
+        if ($page < $page_count) {
+            $data["next"] = $next_url;
+        }
+        $data["data"] = $result;
+        return $data;
+    }
+
+    function get_woocommerce_memberships(WP_REST_Request $request) {
+        global $wp;
+        global $wpdb;
+        $per_page = intval($request->get_param( "per_page") ?? 10);
+        $page = intval($request->get_param( "page") ?? 1);
+        $args = ([
+            'post_type'   => 'wc_user_membership',
+            'posts_per_page' => $per_page,
+            'offset'      => ($page - 1) * $per_page,
+            'order'       => 'ASC',
+            'orderby'     => "modified",
+            'no_found_rows' => false,
+            "post_status" => array("any")
+        ]);
+        if (!empty($request->get_param( "modified_after"))) {
+            $args["date_query"] = array(
+                array(
+                    'column'     => 'post_modified_gmt',
+                    'after'      => $request->get_param( "modified_after"),
+                ),
+            );
+        }
+        $wp_query = new WP_Query($args);
+        $posts = $wp_query->posts;
+        $count = intval($wp_query->found_posts);
+        $page_count = ceil(intval($count) / $per_page);
+        if ( empty( $posts ) ) {
+            return null;
+        }
+        $result = [];
+        foreach ($posts as $post) {
+            try {
+                $sql = "SELECT * FROM wp_postmeta WHERE post_id={$post->ID}";
+                $post_meta = $wpdb->get_results($sql);
+                foreach($post_meta as $meta) {
+                    $val = $this->isSerialized($meta->meta_value) ? 
+                        unserialize($meta->meta_value) : 
+                        $meta->meta_value;
+                    $post->{$meta->meta_key} = $val;
+                }
+                $post->customer_id = $post->post_author;
+                $post->status = str_replace("wcm-", "", $post->post_status);
+                $post->start_date = $post->_start_date;
+                $post->end_date = $post->_end_date;
+                $post->cancelled_date = $post->_cancelled_date;
+                $post->paused_intervals = [];
+                foreach ($post->_paused_intervals as $start => $end) {
+                    $obj = new StdClass();
+                    $obj->start = $this->convert_to_date($start);
+                    if ($end) {
+                        $obj->end = $this->convert_to_date($end);
+                    }
+                    $post->paused_intervals[] = $obj;
+                }
+                $post->date_created = $post->post_date_gmt;
+                $post->date_modified = $post->post_modified_gmt;
+                $order = wc_get_order( $post->_order_id );
+                $post->order = $this->normalise_fields($this->filter_fields($order->get_data(), $this->order_filtered_fields));
+                $product = wc_get_product($post->_product_id);
+                $post->product = $this->normalise_fields($this->filter_fields($product->get_data(), $this->product_filtered_fields));
+                $post = $this->filter_fields($post, $this->membership_filtered_fields);
+                $post = $this->normalise_fields($post);
+                $result[] = $post;
+            } catch(Exception $err) {
+                trigger_error($err, E_USER_WARNING);
+            }
         }
         $next_url = add_query_arg( ["page" => $page + 1, "per_page" => $per_page], home_url($wp->request) );
         $prev_url = add_query_arg( ["page" => $page - 1, "per_page" => $per_page], home_url($wp->request) );
